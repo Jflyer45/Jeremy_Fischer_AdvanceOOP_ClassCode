71cbcf1446cdcebb089065b4a7f813b2c453563b
Jeremy Fischer <jeremyfischer52@gmail.com>
2023-01-12 18:06:08 -0600

    Observer class setup

---
+++ ObserverLearning/ObserverLearning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObserverLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
