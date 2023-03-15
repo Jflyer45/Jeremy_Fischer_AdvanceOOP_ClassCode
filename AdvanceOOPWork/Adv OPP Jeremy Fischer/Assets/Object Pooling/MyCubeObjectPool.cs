675936cb71df8ceae7778b5aca3680f6764ad9f5
Jeremy Fischer <jeremyfischer52@gmail.com>
2023-03-14 19:21:21 -0500

    Pool script work

--- AdvanceOOPWork/Adv OPP Jeremy Fischer/Assets/Object Pooling/MyCubeObjectPool.cs
+++ AdvanceOOPWork/Adv OPP Jeremy Fischer/Assets/Object Pooling/MyCubeObjectPool.cs
@@ -7,17 +7,55 @@ public class MyCubeObjectPool : MonoBehaviour
 {
     public int maxPoolSize = 20;
     public int stackDefaultCapicity = 20;
+    public IObjectPool<MyCube> pool;
 
-
-    // Start is called before the first frame update
-    void Start()
+    public IObjectPool<MyCube> Pool
     {
-        
+        get 
+        {  
+            if(pool == null)
+            {
+                pool = new ObjectPool<MyCube>(CreatedPooledItem, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject, true, maxPoolSize);
+            }
+            return pool;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private MyCube CreatedPooledItem()
     {
-        
+        GameObject go = Instantiate(Resources.Load("Cube1", typeof(GameObject))) as GameObject;
+
+        MyCube cube = go.AddComponent<MyCube>();
+        go.name = "My Cube";
+
+        cube.Pool = Pool;
+        return cube;
+    }
+
+    private void OnTakeFromPool(MyCube cube)
+    {
+        cube.gameObject.SetActive(true);
+    }
+
+    private void OnDestroyPoolObject(MyCube cube)
+    {
+        Destroy(cube.gameObject);
+    }
+
+    private void OnReturnedToPool(MyCube cube)
+    {
+        cube.gameObject.SetActive(true);
+    }
+
+    public void Spawn()
+    {
+        int amount = Random.Range(1, 10);
+
+        for(int i = 0; i < amount; i++)
+        {
+            var dino = Pool.Get();
+
+            dino.transform.position = Random.insideUnitSphere*10;
+        }
     }
 }
