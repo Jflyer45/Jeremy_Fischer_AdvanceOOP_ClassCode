2707ffd8b0ed20422807db64255f040c7b42f122
Jeremy Fischer <jeremyfischer52@gmail.com>
2023-03-16 19:07:54 -0500

    Adapter pattern code

---
+++ AdvanceOOPWork/Adv OPP Jeremy Fischer/Assets/Adapter Pattern/IInventorySystem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This tells us how we wished the original class worked
+// 
+public interface IInventorySystem
+{
+    void SyncInventories();
+
+    void AddItem(InventoryItem anItem, SaveLocation aLocation);
+    void RemoveItem(InventoryItem anItem, SaveLocation aLocation);
+    List<InventoryItem> GetInventory(SaveLocation aLocation);
+}
---
+++ AdvanceOOPWork/Adv OPP Jeremy Fischer/Assets/Adapter Pattern/InventoryItem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new item", menuName = "Inventory")]
+public class InventoryItem : ScriptableObject
+{
+    // place holder, need to do in assignment
+}
