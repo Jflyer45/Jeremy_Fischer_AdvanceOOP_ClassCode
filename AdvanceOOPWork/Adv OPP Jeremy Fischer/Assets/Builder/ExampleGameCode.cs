67cd84f4eeb1dbb814adfb8e3b3f3d39a3854bf8
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2023-03-19 15:09:23 -0500

    Update ExampleGameCode.cs

--- AdvanceOOPWork/Adv OPP Jeremy Fischer/Assets/Builder/ExampleGameCode.cs
+++ AdvanceOOPWork/Adv OPP Jeremy Fischer/Assets/Builder/ExampleGameCode.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class ExampleGameCode : MonoBehaviour
 {
-    void Start()
+    // See how, no matter the monster, we have the ability to construct how we want?
+    // You could even have a scriptable object that is used when constructing
+    public void GenerateMonster(MonsterBuilder mb, bool hasColor = true)
     {
-        
+        MonsterEngineer myEngineer = new MonsterEngineer(mb);
+        if (hasColor) { myEngineer.ConstructMonster(); }
+        else { myEngineer.ContructMonsterNoColor(); }
+        Monster monster = myEngineer.getMonster();
+
+
+        GameObject myCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        CubeProfile p = myCube.AddComponent<CubeProfile>();
+        p.SetUp(monster.getMonsterName(), monster.getMonsterColor(), monster.getMonsterHealth());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
 
     [ContextMenu("Generate Standard Goblin")]
     public void GenerateStandardGoblin()
@@ -35,20 +43,4 @@ public class ExampleGameCode : MonoBehaviour
         MonsterBuilder standardGoblinBuilder = new RandomGoblinBuilder();
         GenerateMonster(standardGoblinBuilder);
     }
-    
-    // See how, no matter the monster, we have the ability to construct how we want?
-    // You could even have a scriptable object that is used when constructing
-    public void GenerateMonster(MonsterBuilder mb, bool hasColor=true)
-    {
-        MonsterEngineer myEngineer = new MonsterEngineer(mb);
-        if (hasColor) { myEngineer.ConstructMonster(); }
-        else          { myEngineer.ContructMonsterNoColor(); }
-        Monster monster = myEngineer.getMonster();
-
-
-        GameObject myCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        CubeProfile p = myCube.AddComponent<CubeProfile>();
-        p.SetUp(monster.getMonsterName(), monster.getMonsterColor(), monster.getMonsterHealth());
-    }
-
 }
