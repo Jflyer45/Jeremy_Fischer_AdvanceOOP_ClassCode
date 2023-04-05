9079caa0abe22d01c97ca94be50d70f17afd277c
Jeremy Fischer <jeremyfischer52@gmail.com>
2023-04-04 19:27:38 -0500

    Template pattern more work

--- TemplatePattern/TemplatePattern/Program.cs
+++ TemplatePattern/TemplatePattern/Program.cs
@@ -7,7 +7,14 @@ namespace TemplatePattern
     {
         public static void Main(string[] args)
         {
-            
+            FileWriter fw = new DESWriter("C:\\Users\\jflye\\source\\repos\\AdvanceOOP_ClassCode\\TemplatePattern\\TemplatePattern\\encrypted.txt");
+            fw.Append("All work and no plasy makes Jack a dull boy.");
+
+            FileWriter fw2 = new UnencryptedWritercs("C:\\Users\\jflye\\source\\repos\\AdvanceOOP_ClassCode\\TemplatePattern\\TemplatePattern\\unencrypted.txt");
+            fw2.Append("All work and no plasy makes Janice a dull Girl.");
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
         }
     }
 }
---
+++ TemplatePattern/TemplatePattern/UnencryptedWritercs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatePattern
+{
+    public class UnencryptedWritercs : FileWriter
+    {
+        public UnencryptedWritercs(string aPath)
+        {
+            this.path = aPath;
+        }
+
+        protected override void DecryptFile()
+        {
+            // Do nothing on purpose
+        }
+
+        protected override void EncryptFile()
+        {
+            // Don't need to do anything
+        }
+    }
+}
